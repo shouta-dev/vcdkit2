b2c7402931085343413ad7fd3a63d1cdb0982510
Kaoru Fukumoto <k1fukumoto@gmail.com>
2011-12-09 14:53:06 +0900

    Use a new basename

--- cron/vcd-vapp.sh
+++ cron/vcd-vapp.sh
@@ -2,11 +2,11 @@
 
 run() {
   $VCDKIT/script/vcd-vapp.rb \
-    -D --vdc Admin,'Basic - Admin', -nCBMON \
+    -D --vdc Admin,'Basic - Admin', -nVCDKITTEST \
     -l$VCDKIT/log/vcd-vapp-delete.log \
     -t -m $VCDKIT/config/mailer.xml && \
   $VCDKIT/script/vcd-vapp.rb \
-    -A --vapptemplate Admin,'Basic - Admin',CBMON_TMPL -nCBMON  \
+    -A --vapptemplate Admin,'Basic - Admin',VCDKITTEST-TMPL -nVCDKITTEST  \
     -l$VCDKIT/log/vcd-vapp-create.log \
     -t -m $VCDKIT/config/mailer.xml
 }
